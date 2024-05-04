4d83e5ba4c0a91f46aaef46b60a690901e5ec1ea
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-05-04 21:43:55 +0300

    add lesson cluster

---
+++ lessons/os.js
@@ -0,0 +1,20 @@
+const os = require('os')
+const cluster = require('cluster')
+// console.log(os.platform())
+// console.log(os.arch())
+// console.log(os.cpus().length)
+
+if (cluster.isMaster) {
+    for (let i = 0; i < os.cpus().length - 2; i++) {
+        cluster.fork()
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Воркер с pid = ${worker.process.pid} умер`)
+        cluster.fork()
+    })
+} else {
+    console.log(`Воркер с pid = ${process.pid}`)
+    setInterval(()=> {
+        console.log(`Воркер с pid = ${process.pid} еще работает`)
+    }, 5000)
+}
