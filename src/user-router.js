706545fed243bc4d4a992c41765cb46003e25126
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-05-05 01:36:54 +0300

    finish framework

--- src/user-router.js
+++ src/user-router.js
@@ -1,22 +1,9 @@
 const Router = require('../framework/Router')
-
+const controller = require('./user-controller')
 const router = new Router()
-const users = [
-    {id: 1, name: 'Serega'},
-    {id: 2, name: "Alex"}
-]
-router.get('/users', (req, res) => {
-    if (req.params.id) {
-        console.log(req.params.id)
-        return res.send(users.find(user => user.id === +req.params.id))
-    }
-    res.send(users)
-})
 
-router.post('/users', (req, res) => {
-    const user = req.body
-    users.push(user)
-    res.send(users)
-})
+router.get('/users', controller.getUsers)
+
+router.post('/users', controller.createUser)
 
 module.exports = router
